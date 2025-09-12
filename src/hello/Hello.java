191a9ead101f71126061061cebf184f7675ced1b
buttivaishnavi <buttivaishnavi19@gmail.com>
2025-09-12 13:48:32 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("hello hi");
+     System.out.println("working with ngrok");
+    
   }
 }
