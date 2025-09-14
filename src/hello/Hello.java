88090e591ceb84ed5c073f5e52e040950e5c2584
buttivaishnavi <buttivaishnavi19@gmail.com>
2025-09-14 15:01:40 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,9 +2,11 @@ package hello;
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
-    System.out.println("hello hi");
       System.out.println("checking ngrok");
     System.out.println("ngrok is not working");
+    System.out.println("is it working");
+    
+    
     
         
   }
